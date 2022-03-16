e0839589003540b607bf8f7d3fd61ffb7a3b44e8
tqk2811 <tqk2811@gmail.com>
2022-03-16 17:04:25 +0700

    move from wpfui

---
+++ TqkLibrary.Utils/ConvertUnit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ConvertUnit
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static readonly string[] UnitSize = { "Byte", "Kib", "Mib", "Gib", "Tib" };
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly string[] UnitSizeSpeed = { "Byte/s", "Kib/s", "Mib/s", "Gib/s", "Tib/s" };
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="round"></param>
+        /// <param name="units"></param>
+        /// <param name="div"></param>
+        /// <returns></returns>
+        public static string ConvertSize(double num, int round, string[] units, int div = 1024)
+        {
+            if (num == 0) return "0 " + units[0];
+            for (double i = 0; i < units.Length; i++)
+            {
+                double sizeitem = num / Math.Pow(div, i);
+                if (sizeitem < 1 && sizeitem > -1)
+                {
+                    if (i == 0) return "0 " + units[0];
+                    else return Math.Round((num / Math.Pow(div, i - 1)), round).ToString() + " " + units[(int)i - 1];
+                }
+            }
+            return Math.Round(num / Math.Pow(div, units.Length - 1), round).ToString() + " " + units[units.Length - 1];
+        }
+    }
+}
---
+++ TqkLibrary.Utils/RandomStrings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class RandomStrings
+    {
+        private static Random rd = new Random();
+        private const string chars_lower = "abcdefghijklmnopqrstuvwxyz";
+        private const string chars_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string charsAndNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private const string Nums = "0123456789";
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lengthMin"></param>
+        /// <param name="lengthMax"></param>
+        /// <returns></returns>
+        public static string RandomString(int lengthMin, int lengthMax)
+        {
+            return new string(Enumerable.Repeat(chars, rd.Next(lengthMin, lengthMax + 1)).Select(s => s[rd.Next(s.Length)]).ToArray());
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lengthMin"></param>
+        /// <param name="lengthMax"></param>
+        /// <returns></returns>
+        public static string RandomNumber(int lengthMin, int lengthMax)
+        {
+            return new string(Enumerable.Repeat(Nums, rd.Next(lengthMin, lengthMax + 1)).Select(s => s[rd.Next(s.Length)]).ToArray());
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lengthMin"></param>
+        /// <param name="lengthMax"></param>
+        /// <returns></returns>
+        public static string RandomStringAndNum(int lengthMin, int lengthMax)
+        {
+            return new string(Enumerable.Repeat(charsAndNum, rd.Next(lengthMin, lengthMax + 1)).Select(s => s[rd.Next(s.Length)]).ToArray());
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lengthMin"></param>
+        /// <param name="lengthMax"></param>
+        /// <returns></returns>
+        public static string RandomStringUpper(int lengthMin, int lengthMax)
+        {
+            return new string(Enumerable.Repeat(chars_upper, rd.Next(lengthMin, lengthMax + 1)).Select(s => s[rd.Next(s.Length)]).ToArray());
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lengthMin"></param>
+        /// <param name="lengthMax"></param>
+        /// <returns></returns>
+        public static string RandomStringLower(int lengthMin, int lengthMax)
+        {
+            return new string(Enumerable.Repeat(chars_lower, rd.Next(lengthMin, lengthMax + 1)).Select(s => s[rd.Next(s.Length)]).ToArray());
+        }
+    }
+}
