a8334837341ec3c0eaa21c3675b98fb1ca57fc4d
tqk2811 <tqk2811@gmail.com>
2025-01-08 17:17:28 +0700

    add FileInfoHelper.GetFileNameWithoutExtension

---
+++ TqkLibrary.Utils/FileInfoHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class FileInfoHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string GetFileNameWithoutExtension(this FileInfo fileInfo)
+        {
+            if (fileInfo is null) throw new ArgumentNullException(nameof(fileInfo));
+            if (fileInfo.Extension.Length == 0) return fileInfo.Name;
+            return fileInfo.Name.Substring(0, fileInfo.Name.Length - fileInfo.Extension.Length);
+        }
+    }
+}
