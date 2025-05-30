1e20f090c899164f0cca4e68ede7f2f8cfaf0bf4
tqk2811 <tqk2811@gmail.com>
2025-05-30 16:56:08 +0700

    add StringHelpers

---
+++ TestProject/UriHelpersTest.cs
@@ -0,0 +1,26 @@
+﻿//using TqkLibrary.Utils;
+
+//namespace TestProject
+//{
+//    [TestClass]
+//    public sealed class UriHelpersTest
+//    {
+//        public static IEnumerable<object[]> Datas
+//        {
+//            get
+//            {
+//                yield return new object[] { new Uri("https://www.facebook.com/?sk=nf"), "www.facebook.com" };
+//                yield return new object[] { new Uri("https://localhost:2132/"), "localhost:2132" };
+//                yield return new object[] { new Uri("https://127.0.0.1:2132/"), "127.0.0.1:2132" };
+//                yield return new object[] { new Uri("https://abc:313s@localhost:2132/"), "localhost:2132" };
+//            }
+//        }
+
+
+//        [TestMethod, DynamicData(nameof(Datas))]
+//        public void TestMethod(Uri uri, string? domain)
+//        {
+
+//        }
+//    }
+//}
---
+++ TqkLibrary.Utils/StringHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StringHelpers
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enumerable"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string Join(this IEnumerable<string> enumerable, string separator)
+        {
+            if (string.IsNullOrWhiteSpace(separator)) throw new ArgumentNullException(nameof(separator));
+            if (enumerable is null) throw new ArgumentNullException(nameof(enumerable));
+            return string.Join(separator, enumerable);
+        }
+    }
+}
