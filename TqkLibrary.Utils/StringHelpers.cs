e65f4f52d28d268506b74892cc015be733f8265f
tqk2811 <tqk2811@gmail.com>
2025-05-30 17:26:23 +0700

    StringHelpers.Contains

--- TqkLibrary.Utils/StringHelpers.cs
+++ TqkLibrary.Utils/StringHelpers.cs
@@ -21,5 +21,31 @@ namespace TqkLibrary.Utils
             if (enumerable is null) throw new ArgumentNullException(nameof(enumerable));
             return string.Join(separator, enumerable);
         }
+
+#if NETSTANDARD2_0
+        //.NET: Core 2.1, Core 2.2, Core 3.0, Core 3.1, 5, 6, 7, 8, 9, 10
+        //NET Standard: 2.1
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="value"></param>
+        /// <param name="stringComparison"></param>
+        /// <returns></returns>
+        public static bool Contains(this string text, string value, StringComparison stringComparison = default)
+        {
+            return text.IndexOf(value, stringComparison) >= 0;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="value"></param>
+        /// <param name="stringComparison"></param>
+        /// <returns></returns>
+        public static bool Contains(this string text, char value, StringComparison stringComparison = default)
+            => text.Contains(value.ToString(), stringComparison);
+#endif
+
     }
 }
