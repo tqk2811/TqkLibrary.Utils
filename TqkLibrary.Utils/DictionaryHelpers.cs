f1bd394f5c163d1113ce1c70c748f61673f0b02f
tqk2811 <tqk2811@gmail.com>
2025-06-02 16:43:20 +0700

    add DictionaryHelpers.TryGetValue

---
+++ TqkLibrary.Utils/DictionaryHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DictionaryHelpers
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static TValue? TryGetValue<TKey, TValue>(IReadOnlyDictionary<TKey, TValue> dict, TKey key)
+        {
+            if (dict.TryGetValue(key, out TValue? value))
+            {
+                return value;
+            }
+            else
+            {
+                return default(TValue);
+            }
+        }
+    }
+}
