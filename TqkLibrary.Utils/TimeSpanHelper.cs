0d6fcc46dacd124bad35728636473bd2da83c089
tqk2811 <tqk2811@gmail.com>
2025-09-08 18:17:16 +0700

    add TimeSpanHelper

---
+++ TqkLibrary.Utils/TimeSpanHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class TimeSpanHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timeSpan"></param>
+        /// <returns></returns>
+        public static TimeSpan GetTimeInDay(this TimeSpan timeSpan)
+        {
+            while (timeSpan.TotalDays >= 1.0)
+            {
+                timeSpan -= TimeSpan.FromDays(1);
+            }
+            return timeSpan;
+        }
+    }
+}
