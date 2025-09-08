3412f146a618ebcff491e546afa4852365466cd5
tqk2811 <tqk2811@gmail.com>
2025-09-08 17:46:09 +0700

    add RandomNext

---
+++ TqkLibrary.Utils/RandomNext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TqkLibrary.Utils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class RandomNext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static TimeSpan Next(this Random random, TimeSpan min, TimeSpan max)
+        {
+            if (min == max) return min;
+            if (max < min) throw new ArgumentException("max must large than min");
+
+            double diff = (max - min).TotalSeconds;
+            return min + TimeSpan.FromSeconds(random.NextDouble() * diff);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static DateTime Next(this Random random, DateTime min, DateTime max)
+        {
+            if (min == max) return min;
+            if (max < min) throw new ArgumentException("max must large than min");
+
+            double diff = (max - min).TotalSeconds;
+            return min + TimeSpan.FromSeconds(random.NextDouble() * diff);
+        }
+    }
+}
