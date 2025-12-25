6b4308c197d0cafb37ffd6cd883463be24100e51
tqk2811 <tqk2811@gmail.com>
2025-12-25 18:02:10 +0700

    add SystemDrawingHelper

---
+++ TqkLibrary.Utils/SystemDrawingHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TqkLibrary.Utils
+{
+    public static class SystemDrawingHelper
+    {
+        public static Point? GetCenter(this Rectangle? rectangle)
+        {
+            if (!rectangle.HasValue) return null;
+            return rectangle.Value.GetCenter();
+        }
+        public static Point GetCenter(this Rectangle rectangle)
+        {
+            if (rectangle.IsEmpty) return Point.Empty;
+            int x = rectangle.X + rectangle.Width / 2;
+            int y = rectangle.Y + rectangle.Height / 2;
+            return new Point(x, y);
+        }
+    }
+}
