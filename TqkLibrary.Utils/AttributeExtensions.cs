bfbf2d2157d4b94ab983dae1669dd32e438eeb2c
tqk2811 <tqk2811@gmail.com>
2026-03-20 15:57:22 +0700

    add GetAttributes

--- TqkLibrary.Utils/AttributeExtensions.cs
+++ TqkLibrary.Utils/AttributeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 namespace TqkLibrary.Utils
@@ -14,22 +15,31 @@ namespace TqkLibrary.Utils
         /// <typeparam name="TAttribute"></typeparam>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static TAttribute? GetAttribute<TAttribute>(this Enum value) where TAttribute : Attribute
+        public static IEnumerable<TAttribute> GetAttributes<TAttribute>(this Enum value) where TAttribute : Attribute
         {
             Type type = value.GetType();
             string? name = Enum.GetName(type, value);
             if (string.IsNullOrWhiteSpace(name))
             {
-                return null;
+                return Enumerable.Empty<TAttribute>();
             }
 
             FieldInfo? field = type.GetField(name);
             if (field is null)
             {
-                return null;
+                return Enumerable.Empty<TAttribute>();
             }
 
-            return field.GetCustomAttributes(false).OfType<TAttribute>().SingleOrDefault();
+            return field.GetCustomAttributes(false).OfType<TAttribute>();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TAttribute"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static TAttribute? GetAttribute<TAttribute>(this Enum value) where TAttribute : Attribute
+            => value.GetAttributes<TAttribute>().SingleOrDefault();
     }
 }
